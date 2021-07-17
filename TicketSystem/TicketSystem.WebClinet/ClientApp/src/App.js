5319124f5f799d829f3b106a56f3677160919f73
Eben Burger <eben@arlowconsulting.co.za>
2021-07-17 13:09:21 +0200

    custom styling sheets added

--- TicketSystem/TicketSystem.WebClinet/ClientApp/src/App.js
+++ TicketSystem/TicketSystem.WebClinet/ClientApp/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home  from './components/home/home';
+import Navbar from "./components/layout/navbar/navbar";
+
+import './custom.xs.css'
+import './custom.sm.css'
+import './custom.md.css'
+import './custom.lg.css'
+import './custom.xl.css'
 
 const App = () => {
   return (
-      <Switch>
-          <Route path='/' component={Home} />
-      </Switch>
+      <>
+          <Navbar />
+          <Switch>
+              <Route path='/' component={Home} />
+          </Switch>
+      </>
   )
 }
 
