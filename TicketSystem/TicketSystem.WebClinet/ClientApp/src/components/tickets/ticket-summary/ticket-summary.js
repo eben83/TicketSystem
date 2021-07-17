6da7bf3f77c88eed25b427ff7066e3436648ed03
Eben Burger <eben@arlowconsulting.co.za>
2021-07-17 16:10:32 +0200

    updated ticket summary layout

--- TicketSystem/TicketSystem.WebClinet/ClientApp/src/components/tickets/ticket-summary/ticket-summary.js
+++ TicketSystem/TicketSystem.WebClinet/ClientApp/src/components/tickets/ticket-summary/ticket-summary.js
@@ -8,12 +8,13 @@ const TicketSummary = (props) => {
             <div className="ticket-summary card m-2">
                 <div className="card-body text-center">
                     <div className='h3'>Ticket</div>
-                    <p>this is the number</p>
+                    <p>1234</p>
+                    <div className='d-flex justify-content-between'>
+                        <p>created on:</p>
+                        <p>1 July 2021</p>
+                    </div>
                 </div>
-                <div className='card-footer d-flex justify-content-between'>
-                    <p>created on:</p>
-                    <p>1 July 2021</p>
-                </div>
+                
             </div>
         </>
     );
