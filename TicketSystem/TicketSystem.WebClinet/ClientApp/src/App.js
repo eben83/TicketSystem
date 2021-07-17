056e407c3d79ea3c4b98648f84ffe3f9974d1ab6
Eben Burger <eben@arlowconsulting.co.za>
2021-07-17 16:47:55 +0200

    sign in form created, styling sheets added

--- TicketSystem/TicketSystem.WebClinet/ClientApp/src/App.js
+++ TicketSystem/TicketSystem.WebClinet/ClientApp/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home  from './components/home/home';
 import Navbar from "./components/layout/navbar/navbar";
+import TicketDetail from "./components/tickets/ticket-detail/ticket-detail";
+import SignIn from './components/auth/signIn'
 
 import './custom.xs.css'
 import './custom.sm.css'
 import './custom.md.css'
 import './custom.lg.css'
 import './custom.xl.css'
-import TicketDetail from "./components/tickets/ticket-detail/ticket-detail";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Switch>
               <Route exact path='/' component={Home} />
               <Route path='/ticket/:id' component={TicketDetail} />
+              <Route path='/signin' component={SignIn} />
           </Switch>
       </>
   )
--- TicketSystem/TicketSystem.WebClinet/ClientApp/src/components/auth/signIn.js
+++ TicketSystem/TicketSystem.WebClinet/ClientApp/src/components/auth/signIn.js
@@ -1,9 +1,61 @@
-import React, {} from 'react';
+import React, {Component} from 'react';
 
-const SignIn = (props) => {
-    return (
-        <>
-        </>
-    );
+import './signIn.xs.css'
+import './signIn.sm.css'
+import './signIn.md.css'
+import './signIn.lg.css'
+import './SignIn.xl.css'
+
+class SignIn extends Component {
+    
+    state = {
+        email: '',
+        password: ''
+    }
+    
+    handleChange = (e) => {
+        this.setState ({
+            [e.target.id]: e.target.value
+        })
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+    
+    render() {
+        return (
+            <>
+                <div className='sign-in p-4'>
+                    <form onSubmit={this.handleSubmit} className='d-flex justify-content-center align-items-center'>
+                        <div>
+                            <h1>Sign In</h1>
+                            <div className='d-flex flex-column mt-5'>
+                                <input
+                                    type='email'
+                                    id='email'
+                                    placeholder='Email'
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className='mt-3'>
+                                <input
+                                    type='password'
+                                    id='password'
+                                    placeholder='password'
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className='mt-3 d-flex justify-content-center'>
+                                <button className='btn badge-primary'>Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </>
+        )
+    }
 }
+
 export default SignIn;
