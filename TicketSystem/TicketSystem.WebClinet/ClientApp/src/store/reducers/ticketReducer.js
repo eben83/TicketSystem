ff3c66fbab815207aa5e46215fbac3cc0db867e2
Eben Burger <eben@arlowconsulting.co.za>
2021-07-18 12:40:56 +0200

    moved reducers to own folder

---
+++ TicketSystem/TicketSystem.WebClinet/ClientApp/src/store/reducers/ticketReducer.js
@@ -0,0 +1,16 @@
+﻿const initialState = {
+    tickets: [
+        {id: '1', title: 'IT Issue', comment: 'blah blah blah'},
+        {id: '2', title: 'IT Issue', comment: 'blah blah blah'},
+        {id: '3', title: 'Sales Request', comment: 'blah blah blah'},
+        {id: '4', title: 'Sales Request', comment: 'blah blah blah'},
+        {id: '5', title: 'Account Dispute', comment: 'blah blah blah'},
+        {id: '6', title: 'Account Dispute', comment: 'blah blah blah'},
+    ]
+}
+
+const ticketReducer = (state = initialState, action) => {
+    return state
+}
+
+export default ticketReducer
