c16c1192cb891c7a29f2952c4958a9bf0eb0bd18
Eben Burger <eben@arlowconsulting.co.za>
2021-07-26 08:40:58 +0200

    added new prop with firebaseReducer

--- TicketSystem/TicketSystem.WebClinet/ClientApp/src/store/reducers/rootReducer.js
+++ TicketSystem/TicketSystem.WebClinet/ClientApp/src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@
 import ticketReducer from "./ticketReducer";
 import { combineReducers } from 'redux'
 import { firestoreReducer } from "redux-firestore";
+import {firebaseReducer} from "react-redux-firebase";
 
 const rootReducers = combineReducers ({
     auth: authReducer,
     ticket: ticketReducer,
-    firestore: firestoreReducer
+    firestore: firestoreReducer,
+    firebase: firebaseReducer
 })
 
 export default rootReducers
