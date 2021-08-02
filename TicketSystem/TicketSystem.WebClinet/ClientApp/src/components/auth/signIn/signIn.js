7fa143bb4ad4efa7c8973167ebc6403af048dc86
Eben Burger <eben@arlowconsulting.co.za>
2021-08-02 15:45:17 +0200

    added usernames

--- TicketSystem/TicketSystem.WebClinet/ClientApp/src/components/auth/signIn/signIn.js
+++ TicketSystem/TicketSystem.WebClinet/ClientApp/src/components/auth/signIn/signIn.js
@@ -62,6 +62,40 @@ class SignIn extends Component {
                             </div>
                         </div>
                     </form>
+                    <div>
+                        <div className='d-flex justify-content-between'>
+                            <div className='d-flex flex-column'>
+                                <div className='d-flex'>
+                                    <p className='mr-1'>User Name:</p>
+                                    <p>john@test.com</p>
+                                </div>
+                                <div className='d-flex'>
+                                    <p className='mr-1'>Password:</p>
+                                    <p>test1234</p>
+                                </div>
+                            </div>
+                            <div className='d-flex flex-column'>
+                                <div className='d-flex'>
+                                    <p className='mr-1'>User Name:</p>
+                                    <p>smith@test.com</p>
+                                </div>
+                                <div className='d-flex'>
+                                    <p className='mr-1'>Password:</p>
+                                    <p>test1234</p>
+                                </div>
+                            </div>
+                            <div className='d-flex flex-column'>
+                                <div className='d-flex'>
+                                    <p className='mr-1'>User Name:</p>
+                                    <p>paul@account.com</p>
+                                </div>
+                                <div className='d-flex'>
+                                    <p className='mr-1'>Password:</p>
+                                    <p>test1234</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         )
